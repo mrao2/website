2eb96d70e65b19932729fc48c8575ec8556ae993
mrao2 <mohith.rao96@gmail.com>
2024-05-27 10:53:58 -0500

    refresh - start new

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/website/',
-  plugins: [
-    sveltekit(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'My Personal Website',
-        short_name: 'PersonalWeb',
-        description: 'My personal website',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
+	plugins: [sveltekit()]
 });
