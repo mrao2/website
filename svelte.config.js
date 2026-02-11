fa20b761f1acb6aa742d6f8ef78abc31d835e2f4
mrao2 <mohith.rao96@gmail.com>
2026-02-10 18:18:54 -0600

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const isDev = process.argv.includes('dev');
-const base = isDev ? '' : (process.env.BASE_PATH ?? '');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -23,7 +20,7 @@ const config = {
 
 		}),
 		paths: {
-			base
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
